09671f2284bb66f1cf0b18ce9cf30438d1a737d6
Gurpreet Singh <gs109690@gmail.com>
2022-08-11 14:40:09 -0500

    script update

--- scripts/run.js
+++ scripts/run.js
@@ -8,6 +8,24 @@ async function main() {
 
   await timeLock.deployed();
   console.log(`Contract deployed to : ${timeLock.address}`);
+
+  // Passing value in deposit function
+  const passValue = { value: ethers.utils.parseEther("1") };
+  const depositTxn = await timeLock.deposit(passValue);
+  await depositTxn.wait(1);
+
+  const balance = await timeLock.getBalances();
+
+  console.log(`Your locked balance is : ${balance} ether`);
+
+  // Testing withdraw function
+
+  const withdrawTxn = await timeLock.withdraw();
+  await withdrawTxn.wait(1);
+
+  console.log("Withdrawing ether...");
+
+  console.log(`Your locked balance is : ${balance} ether`);
 }
 
 main()
